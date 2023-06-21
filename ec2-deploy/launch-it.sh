b38af0fd71f3147ba6346e8b56134b56bfad6910
Kostis Kapelonis <kostis@codefresh.io>
2023-06-21 12:15:47 +0300

    Don't paginate

--- ec2-deploy/launch-it.sh
+++ ec2-deploy/launch-it.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 IMAGE_ID=$(aws ec2 describe-images --filters Name=name,Values=kostis-demo | jq -r .Images[0].ImageId)
-aws ec2 run-instances --image-id $IMAGE_ID --count 1 --instance-type t2.micro --security-group-ids sg-077847d2f63340b3f --user-data file://scripts/startup.sh --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=from-packer}]'
+aws ec2 run-instances --image-id $IMAGE_ID --count 1 --no-cli-pager --instance-type t2.micro --security-group-ids sg-077847d2f63340b3f --user-data file://scripts/startup.sh --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=from-packer}]'
